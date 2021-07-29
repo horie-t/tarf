07cb5678f5cebf5c609860bb100ee14a48f217f4
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-07-29 19:50:34 +0900

    内蔵LEDでLチカ

---
+++ firmware/l-chika/src/main.rs
@@ -0,0 +1,50 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use embedded_hal::blocking::delay::DelayMs;
+
+use wio_terminal::entry;
+use wio_terminal::hal::clock::GenericClockController;
+use wio_terminal::hal::delay::Delay;
+use wio_terminal::hal::gpio::{Port, Pa15, Input, Floating, Output, PushPull};
+use wio_terminal::pac::{CorePeripherals, Peripherals};
+
+struct Led {
+    pin: Pa15<Output<PushPull>>,
+}
+
+impl Led {
+    fn new(pin: Pa15<Input<Floating>>, port: &mut Port) -> Led {
+        Led {
+            pin: pin.into_push_pull_output(port),
+        }
+    }
+
+    pub fn toggle(&mut self) {
+        self.pin.toggle();
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+
+    let mut pins = wio_terminal::Pins::new(peripherals.PORT);
+    let mut led = Led::new(pins.user_led, &mut pins.port);
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    loop {
+        delay.delay_ms(1_000u16);
+        led.toggle();
+    }
+}
