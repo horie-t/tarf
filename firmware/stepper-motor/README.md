969bed555d57076d7b29135bb213fc74833fe057
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-08-11 19:09:21 +0900

    ステッピングモータのSPIの初期化コードを実装

---
+++ firmware/stepper-motor/README.md
@@ -0,0 +1,18 @@
+# Wio Terminalとブレッドボードを接続して、ステッピングモータを動かします。
+
+## ブレッドボードとの接続
+
+Wio Teraminlのピンは、説明書参照。
+
+| ピンNo. | 説明     | ASTAMD51P19A パッド |
+| ------- | -------- | ------------------  |
+| 19      | SPI_MOSI | PB02/SERCOM5.0      |
+| 21      | SPI_MISO | PB00/SERCOM5.2      |
+| 23      | SPI_SCLK | PB03/SERCOM5.1      |
+| 24      | SPI_CS   | PB01/SERCOM5.3      |
+
+## 参考情報
+
+* [L6470使用　ステッピングモータードライブキットの取扱説明書](https://akizukidenshi.com/download/ds/akizuki/AE-L6470_20190118.pdf)
+* [L6470データシート](https://akizukidenshi.com/download/ds/st/L6470.pdf)
+* [Aruduinoでのステッピングモータの接続](https://github.com/laurb9/StepperDriver)
---
+++ firmware/stepper-motor/src/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use wio_terminal::entry;
+use wio_terminal::hal::clock::GenericClockController;
+use wio_terminal::hal::gpio::*;
+use wio_terminal::hal::hal::spi;
+use wio_terminal::hal::sercom::*;
+use wio_terminal::pac::{CorePeripherals, Peripherals};
+use wio_terminal::prelude::*;
+
+#[entry]
+fn main() -> ! {
+    // 初期化処理
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+
+    let mut pins = wio_terminal::Pins::new(peripherals.PORT);
+    let gclk0 = &clocks.gclk0();
+    let mut spi: SPIMaster5<
+        Sercom5Pad2<Pb0<PfD>>,
+        Sercom5Pad0<Pb2<PfD>>,
+        Sercom5Pad1<Pb3<PfD>>,
+    > = SPIMaster5::new(
+        &clocks.sercom5_core(&gclk0).unwrap(),
+        8.mhz(),
+        spi::MODE_3,
+        peripherals.SERCOM5,
+        &mut peripherals.MCLK,
+        (
+            pins.spi_miso.into_pad(&mut pins.port),
+            pins.spi_mosi.into_pad(&mut pins.port),
+            pins.spi_sck.into_pad(&mut pins.port),
+        ),
+    );
+
+    loop {
+
+    }
+}
