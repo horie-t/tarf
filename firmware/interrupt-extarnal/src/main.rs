9d46d9d5b4343ae4b226e646f8760de9314611e3
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-08-29 12:30:27 +0900

    LEDの構造体をジェネリック化

--- firmware/interrupt-extarnal/src/main.rs
+++ firmware/interrupt-extarnal/src/main.rs
@@ -13,18 +13,17 @@ use wio_terminal::hal::gpio::*;
 use wio_terminal::pac::{CorePeripherals, Peripherals};
 use wio_terminal::prelude::*;
 
-struct LedExternal {
-    pin: Pa17<Output<PushPull>>,
+struct Led<I: v2::PinId> {
+    pin: v1::Pin<I, Output<PushPull>>,
 }
 
-
-impl LedExternal {
-    fn new(pin: Pa17<Input<Floating>>, port: &mut Port) -> LedExternal {
-        LedExternal {
+impl<I: v2::PinId> Led<I> {
+    fn new(pin: v1::Pin<I, Input<Floating>>, port: &mut Port) -> Led<I> {
+        Led {
             pin: pin.into_push_pull_output(port),
         }
     }
-
+    
     pub fn set_high(&mut self) {
         self.pin.set_high().unwrap();
     }
@@ -41,36 +40,6 @@ impl LedExternal {
         self.pin.toggle();
     }
 }
-
-struct LedInternal {
-    pin: Pa15<Output<PushPull>>,
-}
-
-
-impl LedInternal {
-    fn new(pin: Pa15<Input<Floating>>, port: &mut Port) -> LedInternal {
-        LedInternal {
-            pin: pin.into_push_pull_output(port),
-        }
-    }
-
-    pub fn set_high(&mut self) {
-        self.pin.set_high().unwrap();
-    }
-
-    pub fn set_low(&mut self) {
-        self.pin.set_low().unwrap();
-    }
-
-    pub fn is_set_high(&mut self) -> bool {
-        self.pin.is_set_high().unwrap()
-    }
-
-    pub fn toggle(&mut self) {
-        self.pin.toggle();
-    }
-}
-
 
 #[entry]
 fn main() -> ! {
@@ -85,9 +54,9 @@ fn main() -> ! {
     );
 
     let mut pins = wio_terminal::Pins::new(peripherals.PORT);
-    let mut led_external = LedExternal::new(pins.i2c1_sda, &mut pins.port);
+    let mut led_external = Led::new(pins.i2c1_sda, &mut pins.port);
     led_external.set_low();
-    let mut led_internal = LedInternal::new(pins.user_led, &mut pins.port);
+    let mut led_internal = Led::new(pins.user_led, &mut pins.port);
     led_internal.set_low();
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
