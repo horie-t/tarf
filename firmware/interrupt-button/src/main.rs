85b98a90b4be9bcea8158b95bea75ca2dd89df86
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-09-05 17:52:41 +0900

    ボタン操作の割込みのボタンを1つに絞る

--- firmware/interrupt-button/src/main.rs
+++ firmware/interrupt-button/src/main.rs
@@ -5,14 +5,16 @@ use panic_halt as _;
 use wio_terminal as wio;
 
 use wio::hal::clock::GenericClockController;
-use wio::hal::gpio::{Port, Pa15, Input, Floating, Output, PushPull};
-use wio::pac::{interrupt, CorePeripherals, Peripherals};
+use wio::hal::common::eic;
+use wio::hal::common::eic::pin::*;
+use wio::hal::common::gpio::*;
+use wio::pac::{interrupt, EIC, MCLK, CorePeripherals, Peripherals};
 use wio::prelude::*;
-use wio::{button_interrupt, entry, ButtonController, ButtonEvent, Pins, Sets};
+use wio::{entry, Button, ButtonEvent, Pins, Sets};
 
 use cortex_m::interrupt::{free as disable_interrupts, CriticalSection};
+use cortex_m::peripheral::NVIC;
 
-use heapless::consts::U8;
 use heapless::spsc::Queue;
 
 struct Led {
@@ -39,6 +41,72 @@ impl Led {
     }
 }
 
+struct ButtonPins {
+    /// button3 pin
+    pub button3: Pc28<Input<Floating>>,
+}
+
+impl ButtonPins {
+    pub fn init(
+        self,
+        eic: EIC,
+        clocks: &mut GenericClockController,
+        mclk: &mut MCLK,
+        port: &mut Port,
+    ) -> ButtonController {
+        let clk = clocks.gclk1();
+        let mut eic = eic::init_with_ulp32k(mclk, clocks.eic(&clk).unwrap(), eic);
+        eic.button_debounce_pins(&[
+            self.button3.id(),
+
+        ]);
+
+        let mut b3 = self.button3.into_floating_ei(port);
+        b3.sense(&mut eic, Sense::BOTH);
+        b3.enable_interrupt(&mut eic);
+        ButtonController {
+            _eic: eic.finalize(),
+            b3,
+        }
+    }
+}
+
+pub struct ButtonController {
+    _eic: eic::EIC,
+    b3: ExtInt12<Pc28<Interrupt<Floating>>>,
+}
+
+macro_rules! isr {
+    ($Handler:ident, $($Event:expr, $Button:ident),+) => {
+        pub fn $Handler(&mut self) -> Option<ButtonEvent> {
+            $(
+                {
+                    let b = &mut self.$Button;
+                    if b.is_interrupt() {
+                        b.clear_interrupt();
+                        return Some(ButtonEvent {
+                            button: $Event,
+                            down: !b.state(),
+                        })
+                    }
+                }
+            )+
+
+            None
+        }
+    };
+}
+
+impl ButtonController {
+    pub fn enable(&self, nvic: &mut NVIC) {
+        unsafe {
+            nvic.set_priority(interrupt::EIC_EXTINT_12, 1);
+            NVIC::unmask(interrupt::EIC_EXTINT_12);
+        }
+    }
+
+    isr!(interrupt_extint12, Button::TopLeft, b3);
+}
 
 #[entry]
 fn main() -> ! {
@@ -56,12 +124,16 @@ fn main() -> ! {
     let pins = Pins::new(peripherals.PORT);
     let mut sets: Sets = pins.split();
 
-    let button_ctrlr = sets.buttons.init(
+    let buttons = ButtonPins {
+        button3: sets.buttons.button3
+    };
+    let button_ctrlr = buttons.init(
         peripherals.EIC,
         &mut clocks,
         &mut peripherals.MCLK,
         &mut sets.port,
     );
+
     let nvic = &mut core.NVIC;
     disable_interrupts(|_| unsafe {
         button_ctrlr.enable(nvic);
@@ -84,7 +156,32 @@ fn main() -> ! {
 }
 
 static mut BUTTON_CTRLR: Option<ButtonController> = None;
-static mut Q: Queue<ButtonEvent, U8> = Queue(heapless::i::Queue::new());
+static mut Q: Queue<ButtonEvent, 8> = Queue::new();
+
+macro_rules! button_interrupt {
+    ($controller:ident, unsafe fn $func_name:ident ($cs:ident: $cstype:ty, $event:ident: ButtonEvent ) $code:block) => {
+        unsafe fn $func_name($cs: $cstype, $event: ButtonEvent) {
+            $code
+        }
+
+        macro_rules! _button_interrupt_handler {
+            ($Interrupt:ident, $Handler:ident) => {
+                #[interrupt]
+                fn $Interrupt() {
+                    disable_interrupts(|cs| unsafe {
+                        $controller.as_mut().map(|ctrlr| {
+                            if let Some(event) = ctrlr.$Handler() {
+                                $func_name(cs, event);
+                            }
+                        });
+                    });
+                }
+            };
+        }
+
+        _button_interrupt_handler!(EIC_EXTINT_12, interrupt_extint12);
+    };
+}
 
 button_interrupt!(
     BUTTON_CTRLR,
