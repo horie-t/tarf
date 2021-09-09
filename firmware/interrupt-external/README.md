221e70ed9eafa00d2fdc7c8c66a3bc66ce8ea0a6
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-09-09 21:17:37 +0900

    ヘッダ・ピンから割込みをかける

---
+++ firmware/interrupt-external/README.md
@@ -0,0 +1,12 @@
+# Wio Terminalへの外部割込みのテスト
+
+## ブレッドボードとの接続
+
+![ButtonExternal](./images/button_external.png)
+
+Wio Teraminlのピンは、説明書参照。
+
+| ピンNO. | 説明     |
+| ------- | -------- |
+| 6       | Ground   |
+| 13      | 割込み用 |
---
+++ firmware/interrupt-external/src/main.rs
@@ -0,0 +1,192 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use wio_terminal as wio;
+
+use wio::hal::clock::GenericClockController;
+use wio::hal::common::eic;
+use wio::hal::common::eic::pin::*;
+use wio::hal::common::gpio::*;
+use wio::pac::{interrupt, EIC, MCLK, CorePeripherals, Peripherals};
+use wio::prelude::*;
+use wio::{entry, Button, ButtonEvent, Pins, Sets};
+
+use cortex_m::interrupt::{free as disable_interrupts, CriticalSection};
+use cortex_m::peripheral::NVIC;
+
+use heapless::spsc::Queue;
+
+struct Led {
+    pin: Pa15<Output<PushPull>>,
+}
+
+impl Led {
+    fn new(pin: Pa15<Input<Floating>>, port: &mut Port) -> Led {
+        Led {
+            pin: pin.into_push_pull_output(port),
+        }
+    }
+
+    pub fn set_high(&mut self) {
+        self.pin.set_high().unwrap();
+    }
+
+    pub fn set_low(&mut self) {
+        self.pin.set_low().unwrap();
+    }
+
+    pub fn toggle(&mut self) {
+        self.pin.toggle();
+    }
+}
+
+struct ButtonPins {
+    /// button pin
+    pub button: Pb8<Input<Floating>>,
+}
+
+impl ButtonPins {
+    pub fn init(
+        self,
+        eic: EIC,
+        clocks: &mut GenericClockController,
+        mclk: &mut MCLK,
+        port: &mut Port,
+    ) -> ButtonController {
+        let clk = clocks.gclk1();
+        let mut eic = eic::init_with_ulp32k(mclk, clocks.eic(&clk).unwrap(), eic);
+        eic.button_debounce_pins(&[
+            self.button.id(),
+
+        ]);
+
+        let mut b = self.button.into_floating_ei(port);
+        b.sense(&mut eic, Sense::BOTH);
+        b.enable_interrupt(&mut eic);
+        ButtonController {
+            _eic: eic.finalize(),
+            b,
+        }
+    }
+}
+
+pub struct ButtonController {
+    _eic: eic::EIC,
+    b: ExtInt8<Pb8<Interrupt<Floating>>>,
+}
+
+macro_rules! isr {
+    ($Handler:ident, $($Event:expr, $Button:ident),+) => {
+        pub fn $Handler(&mut self) -> Option<ButtonEvent> {
+            $(
+                {
+                    let b = &mut self.$Button;
+                    if b.is_interrupt() {
+                        b.clear_interrupt();
+                        return Some(ButtonEvent {
+                            button: $Event,
+                            down: !b.state(),
+                        })
+                    }
+                }
+            )+
+
+            None
+        }
+    };
+}
+
+impl ButtonController {
+    pub fn enable(&self, nvic: &mut NVIC) {
+        unsafe {
+            nvic.set_priority(interrupt::EIC_EXTINT_8, 1);
+            NVIC::unmask(interrupt::EIC_EXTINT_8);
+        }
+    }
+
+    isr!(interrupt_extint8, Button::TopLeft, b);
+}
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let mut core = CorePeripherals::take().unwrap();
+
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+
+    let pins = Pins::new(peripherals.PORT);
+    let mut sets: Sets = pins.split();
+
+    let buttons = ButtonPins {
+        button: sets.header_pins.a0_d0
+    };
+    let button_ctrlr = buttons.init(
+        peripherals.EIC,
+        &mut clocks,
+        &mut peripherals.MCLK,
+        &mut sets.port,
+    );
+
+    let nvic = &mut core.NVIC;
+    disable_interrupts(|_| unsafe {
+        button_ctrlr.enable(nvic);
+        BUTTON_CTRLR = Some(button_ctrlr);
+    });
+
+    let mut led = Led::new(sets.user_led, &mut sets.port);
+
+    let mut consumer = unsafe { Q.split().1 };
+    loop {
+        if let Some(press) = consumer.dequeue() {
+            if press.down {
+                led.set_high();
+            } else {
+                led.set_low();
+            }
+        }
+    }
+
+}
+
+static mut BUTTON_CTRLR: Option<ButtonController> = None;
+static mut Q: Queue<ButtonEvent, 8> = Queue::new();
+
+macro_rules! button_interrupt {
+    ($controller:ident, unsafe fn $func_name:ident ($cs:ident: $cstype:ty, $event:ident: ButtonEvent ) $code:block) => {
+        unsafe fn $func_name($cs: $cstype, $event: ButtonEvent) {
+            $code
+        }
+
+        macro_rules! _button_interrupt_handler {
+            ($Interrupt:ident, $Handler:ident) => {
+                #[interrupt]
+                fn $Interrupt() {
+                    disable_interrupts(|cs| unsafe {
+                        $controller.as_mut().map(|ctrlr| {
+                            if let Some(event) = ctrlr.$Handler() {
+                                $func_name(cs, event);
+                            }
+                        });
+                    });
+                }
+            };
+        }
+
+        _button_interrupt_handler!(EIC_EXTINT_8, interrupt_extint8);
+    };
+}
+
+button_interrupt!(
+    BUTTON_CTRLR,
+    unsafe fn on_button_event(_cs: &CriticalSection, event: ButtonEvent) {
+        let mut q = Q.split().0;
+        q.enqueue(event).ok();
+    }
+);
