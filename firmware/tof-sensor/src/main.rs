d26581f8e8b4a384e651da9ec05b02081c1b64e1
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-11-06 14:15:35 +0900

    ヘッダ・ピンでI2Cを生成

--- firmware/tof-sensor/src/main.rs
+++ firmware/tof-sensor/src/main.rs
@@ -5,10 +5,12 @@ use panic_halt as _;
 use core::fmt::Write;
 
 use wio_terminal as wio;
+use wio::hal::gpio::*;
 use wio::{Pins, UART, entry};
 use wio::hal::clock::GenericClockController;
 use wio::pac::Peripherals;
 use wio::prelude::*;
+use wio::hal::sercom::{I2CMaster3, PadPin, Sercom3Pad0, Sercom3Pad1};
 
 
 #[entry]
@@ -22,6 +24,7 @@ fn main() -> ! {
         &mut peripherals.OSCCTRL,
         &mut peripherals.NVMCTRL,
     );
+    let gclk0 = &clocks.gclk0();
 
     let mut pins = Pins::new(peripherals.PORT);
 
@@ -35,6 +38,18 @@ fn main() -> ! {
         peripherals.SERCOM2,
         &mut peripherals.MCLK,
         &mut pins.port
+    );
+
+    let i2c: I2CMaster3<
+        Sercom3Pad0<Pa17<PfD>>,
+        Sercom3Pad1<Pa16<PfD>>
+    > = I2CMaster3::new(
+        &clocks.sercom3_core(&gclk0).unwrap(),
+        400.khz(),
+        peripherals.SERCOM3,
+        &mut peripherals.MCLK,
+        pins.i2c1_sda.into_pad(&mut pins.port),
+        pins.i2c1_scl.into_pad(&mut pins.port)
     );
 
     writeln!(&mut serial, "Hello, {}!\r", "tarf").unwrap();
