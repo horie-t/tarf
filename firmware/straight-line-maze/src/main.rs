f7c7bb8646e86b96f833ece1ef423c1deb33c657
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-12-15 18:58:09 +0900

    Wheelの構造体を作成

--- firmware/straight-line-maze/src/main.rs
+++ firmware/straight-line-maze/src/main.rs
@@ -6,7 +6,16 @@ use panic_halt as _;
 use wio_terminal as wio;
 use wio::entry;
 use wio::hal::clock::GenericClockController;
+use wio::hal::gpio::v2::{Output, Pin, PinId, Pins, PushPull};
 use wio::pac::Peripherals;
+use wio::prelude::*;
+
+
+struct Wheel<S: PinId, D: PinId> {
+    id: u32,
+    step_pin: Pin<S, Output<PushPull>>,
+    direction_pin: Pin<D, Output<PushPull>>,
+}
 
 #[entry]
 fn main() -> ! {
@@ -24,6 +33,14 @@ fn main() -> ! {
         .unwrap();
     let timer_clock = clocks.tc2_tc3(&gclk5).unwrap();
     let timer_clock1 = clocks.tc4_tc5(&gclk5).unwrap();
+
+    let pins = Pins::new(peripherals.PORT);
+        
+    let wheel_0 = Wheel {
+        id: 0,
+        step_pin: pins.pb08.into_push_pull_output(),
+        direction_pin: pins.pb09.into_push_pull_output(),
+    };
 
     loop {
     }
