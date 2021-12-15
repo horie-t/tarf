693ffb32995e80891ac23f33e5856b025eda5c63
HORIE Tetsuya <horie-t@users.noreply.github.com>
2021-12-15 20:49:03 +0900

    走行装置の構造体を作成し、左車輪を時計回りに回転させる

--- firmware/straight-line-maze/src/main.rs
+++ firmware/straight-line-maze/src/main.rs
@@ -6,10 +6,16 @@ use panic_halt as _;
 use wio_terminal as wio;
 use wio::entry;
 use wio::hal::clock::GenericClockController;
+use wio::hal::delay::Delay;
 use wio::hal::gpio::v2::{Output, Pin, PinId, Pins, PushPull};
-use wio::pac::Peripherals;
+use wio::pac::{CorePeripherals, Peripherals};
 use wio::prelude::*;
 
+#[derive(Clone, Copy)]
+enum WheelRotateDirection {
+    ClockWise,
+    CounterClockWise,
+}
 
 struct Wheel<S: PinId, D: PinId> {
     id: u32,
@@ -17,10 +23,32 @@ struct Wheel<S: PinId, D: PinId> {
     direction_pin: Pin<D, Output<PushPull>>,
 }
 
+impl<S: PinId, D: PinId> Wheel<S, D> {
+    fn step(&mut self) {
+        self.step_pin.toggle().unwrap();
+    }
+
+    fn set_rotate_direction(&mut self, dir: WheelRotateDirection) {
+        match dir {
+            WheelRotateDirection::ClockWise => {
+                self.direction_pin.set_high();
+            },
+            WheelRotateDirection::CounterClockWise => {
+                self.direction_pin.set_low();
+            }
+        }
+    }
+}
+
+struct RunningSystem<S0: PinId, D0: PinId> {
+    wheel_0: Wheel<S0, D0>,
+}
+
 #[entry]
 fn main() -> ! {
     // 初期化処理
     let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
     let mut clocks = GenericClockController::with_external_32kosc(
         peripherals.GCLK,
         &mut peripherals.MCLK,
@@ -28,6 +56,7 @@ fn main() -> ! {
         &mut peripherals.OSCCTRL,
         &mut peripherals.NVMCTRL,
     );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
 
     let gclk5 = clocks.get_gclk(wio::pac::gclk::pchctrl::GEN_A::GCLK5)
         .unwrap();
@@ -42,6 +71,14 @@ fn main() -> ! {
         direction_pin: pins.pb09.into_push_pull_output(),
     };
 
+    let mut running_system = RunningSystem {
+        wheel_0,
+    };
+
+    running_system.wheel_0.set_rotate_direction(WheelRotateDirection::ClockWise);
+
     loop {
+        running_system.wheel_0.step();
+        delay.delay_ms(10u32);
     }
 }
